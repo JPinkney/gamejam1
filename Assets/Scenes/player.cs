b2164000e0e29248e5298f7fd72e908753c8df78
jpinkney <josh.pinkney@mail.utoronto.ca>
2019-01-12 12:07:07 -0500

    Added player script

---
+++ Assets/Scenes/player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    public float speed;
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+
+    private bool isGrounded;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 v = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(v * speed);
+    }
+
+}
